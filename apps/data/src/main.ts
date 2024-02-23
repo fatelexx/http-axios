9991316734354d85125b1c902360d46ede33bce1
fatelexx <fatelexx@outlook.com>
2024-02-24 03:53:44 +0800

    feat: mangodb api

--- apps/data/src/main.ts
+++ apps/data/src/main.ts
@@ -1,12 +1,66 @@
-import express from 'express';
+import { VehicleEntity } from '@http-axios/http';
+import express, { Request, Response } from 'express';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3001;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+const vehicles: Array<VehicleEntity> = [
+  {
+    id: 1,
+    year: 2009,
+    make: 'Mazda',
+    model: 'Mazda3',
+    trim: 'AT'
+  }
+];
+
+app.use(express.json());
+
+app.get('/', (_: Request, res: Response<Array<VehicleEntity>>) => {
+  res.json(vehicles);
+});
+
+app.get('/:id', (req: Request, res: Response<VehicleEntity>) => {
+  const id = +req.params.id;
+  const vehicle = vehicles.find(v => v.id == id);
+  res.json(vehicle);
+});
+
+app.post('/', (req: Request<VehicleEntity>, res: Response<number>) => {
+  const newVehicle: VehicleEntity = {...req.body, id: Math.max(...vehicles.map(x => x.id)) + 1};
+  vehicles.push(newVehicle);
+  res.status(201).json(newVehicle.id);
+});
+
+app.put('/:id', (req: Request<VehicleEntity>, res: Response) => {
+  const id = +req.params.id;
+  const request: VehicleEntity = req.body;
+  const vehicle = vehicles.find(v => v.id === id);
+
+  if (!vehicle) {
+    return res.sendStatus(400);
+  }
+  
+  vehicle.year = request.year;
+  vehicle.make = request.make;
+  vehicle.model = request.model;
+  vehicle.trim = request.trim;
+  
+  res.sendStatus(200);
+});
+
+app.delete('/:id', (req: Request, res: Response) => {
+  const id = +req.params.id;
+  const vehicle = vehicles.find(v => v.id === id);
+
+  if (!vehicle) {
+    return res.sendStatus(400);
+  }
+
+  vehicles.splice(vehicles.indexOf(vehicle), 1);
+  res.sendStatus(200);
 });
 
 app.listen(port, host, () => {
---
+++ libs/http/src/lib/entities.ts
@@ -0,0 +1,8 @@
+// Represents DB Entities (SQL, NOSQL)
+export interface VehicleEntity {
+    id: number;
+    year: number;
+    make: string;
+    model: string;
+    trim: string;
+}
