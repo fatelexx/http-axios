75cdb7d36958b1f1aaa82a81d1b7e6a6d2b07c8f
fatelexx <fatelexx@outlook.com>
2024-02-24 12:19:57 +0800

    chore: backend implementation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,47 @@
-import express from 'express';
+import { CreateVehicleDto, VehicleDto, VehicleEntity } from '@http-axios/http';
+import express, {Response, Request} from 'express';
+import { DbService, MangoDbService } from './mangodb-service';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+const mangoDbService: DbService = new MangoDbService();
+
+app.get('/', async (_, res: Response<Array<VehicleDto>>) => {
+  const vehicles = await mangoDbService.getAll();
+  const result = vehicles.map<VehicleDto>(v => <VehicleDto>{...v})
+  res.send(result);
+});
+
+app.get('/:id', async (req: Request, res: Response<VehicleDto>) => {
+  const id = +req.params.id;
+  const vehicle = await mangoDbService.get(id);
+  res.json(vehicle);
+});
+
+app.post('/', async (req: Request<CreateVehicleDto>, res: Response<number>) => {
+  const createDto: CreateVehicleDto = req.body;
+  const vehicle: VehicleEntity = {...createDto, id: undefined};
+  const id = await mangoDbService.add(vehicle);
+  res.status(201).json(id);
+});
+
+app.put('/:id', async (req: Request<VehicleDto>, res: Response) => {
+  const id = +req.params.id;
+  const vehicleDto: VehicleDto = req.body;
+  const vehicle: VehicleEntity = {...vehicleDto, id};
+  console.log(vehicle);
+  await mangoDbService.update(vehicle);
+  res.sendStatus(200);
+});
+
+app.delete('/:id', async (req: Request, res: Response) => {
+  const id = +req.params.id;
+  await mangoDbService.delete(id);
+  res.sendStatus(200);
 });
 
 app.listen(port, host, () => {
